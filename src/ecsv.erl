42c790efb985bd9c4047f8b3af4bd1ee290c1473
Hynek Vychodil <vychodil.hynek@gmail.com>
2016-07-07 19:12:09 +0200

    More readable function specs in edoc

--- src/ecsv.erl
+++ src/ecsv.erl
@@ -20,7 +20,7 @@
 -export_type([
               state/0
               , options/0
-              , row/0
+              , rows/0
               , line/0
               , input/0
               , reader_state/0
@@ -38,6 +38,7 @@
 
 -type state() :: any().
 -type row() :: tuple().
+-type rows() :: [row()].
 -type line() :: [atom() | number() | iolist()].
 -type input() :: eof | binary().
 
@@ -46,7 +47,7 @@
     fun((State0 :: ReaderStateType) -> State :: ReaderStateType).
 
 -type callback_state() :: any().
--type callback_message() :: {eof | rows, [row()]}.
+-type callback_message() :: {eof | rows, RevRows :: rows()}.
 -type callback_fun(CallBackStateType) ::
     fun((Message :: callback_message(), State0 :: CallBackStateType) ->
         State :: CallBackStateType).
@@ -59,35 +60,55 @@
 default_block_size() ->
     ?BLOCK_SIZE.
 
--spec parse(Bin :: binary()) -> [row()].
+-spec parse(Bin) -> Rows when
+      Bin :: binary(),
+      Rows :: rows().
 parse(Bin) ->
     parse(Bin, []).
 
--spec parse(Bin :: binary(), Opts :: options()) -> [row()].
+-spec parse(Bin, Opts) -> Rows when
+      Bin :: binary(),
+      Opts :: options(),
+      Rows :: rows().
 parse(Bin, Opts) ->
     {ok, Acc, S} = parse_raw(Bin, parser_init(Opts), []),
     {ok, Ls, _} = parse_raw(eof, S, Acc),
     lists:reverse(Ls).
 
--spec parse_step(Input :: input(), State0 :: state()) -> {ok, [row()], State :: state()}.
+-spec parse_step(Input, State0) -> Result when
+      Input :: input(),
+      State0 :: state(),
+      Result :: {ok, Rows, State},
+      Rows :: rows(),
+      State :: state().
 parse_step(Bin, State) ->
     {ok, Ls, S2} = parse_raw(Bin, State, []),
     {ok, lists:reverse(Ls), S2}.
 
--spec parse_stream(
-        reader_fun(ReaderStateType), ReaderState0 :: ReaderStateType,
-        callback_fun(CallBackStateType), CallbackState0 :: CallBackStateType) ->
-    {ReaderState :: ReaderStateType, CallbackState :: CallBackStateType,
-     State :: state()}.
+-spec parse_stream(Reader, ReaderState0, CallBack, CallbackState0) ->
+    Result when
+      Reader :: reader_fun(ReaderStateType),
+      ReaderState0 :: ReaderStateType,
+      CallBack :: callback_fun(CallBackStateType),
+      CallbackState0 :: CallBackStateType,
+      Result :: {ReaderState, CallbackState, State},
+      ReaderState :: ReaderStateType,
+      CallbackState :: CallBackStateType,
+      State :: state().
 parse_stream(Reader, ReaderState0, CallBack, CallbackState0) ->
     parse_stream(Reader, ReaderState0, CallBack, CallbackState0, []).
 
--spec parse_stream(
-        reader_fun(ReaderStateType), ReaderState0 :: ReaderStateType,
-        callback_fun(CallBackStateType), CallbackState0 :: CallBackStateType,
-        StateOrOpts :: state() | options()) ->
-    {ReaderState :: ReaderStateType, CallBackState :: CallBackStateType,
-     State :: state()}.
+-spec parse_stream(Reader, ReaderState0, CallBack, CallbackState0, StateOrOpts) ->
+    Result when
+      Reader :: reader_fun(ReaderStateType),
+      ReaderState0 :: ReaderStateType,
+      CallBack :: callback_fun(CallBackStateType),
+      CallbackState0 :: CallBackStateType,
+      StateOrOpts :: state() | options(),
+      Result :: {ReaderState, CallbackState, State},
+      ReaderState :: ReaderStateType,
+      CallbackState :: CallBackStateType,
+      State :: state().
 parse_stream(Reader, ReaderState0, CallBack, CallbackState0, State) ->
     parse_stream_(Reader, ReaderState0, CallBack, CallbackState0,
                   if
@@ -95,8 +116,13 @@ parse_stream(Reader, ReaderState0, CallBack, CallbackState0, State) ->
                       true           -> State
                   end).
 
--spec parse_raw(Input :: input(), State0 :: state(), Acc :: [row()]) ->
-    {ok, RevRows :: [row()], State :: state()}.
+-spec parse_raw(Input, State0, Acc) -> Result when
+      Input :: input(),
+      State0 :: state(),
+      Acc :: rows(),
+      Result :: {ok, RevRows, State},
+      RevRows :: rows(),
+      State :: state().
 parse_raw(eof, State, Acc) -> ecsv_nif:parse(eof, State, Acc);
 parse_raw(<<Bin:(?BLOCK_SIZE)/bytes, Rest/bytes>>, State, Acc) ->
     {ok, Acc2, S2} = ecsv_nif:parse(Bin, State, Acc),
@@ -106,11 +132,15 @@ parse_raw(<<>>, State, Acc) ->
 parse_raw(<<_/bytes>> = Bin, State, Acc) ->
     ecsv_nif:parse(Bin, State, Acc).
 
--spec parser_init(Opts :: options()) -> state().
+-spec parser_init(Opts) -> State when
+      Opts :: options(),
+      State :: state().
 parser_init(Opts) ->
     ecsv_nif:parser_init(Opts).
 
--spec file_reader() -> reader_fun(file:io_device()).
+-spec file_reader() -> Reader when
+      Reader :: reader_fun(FH),
+      FH :: file:io_device().
 file_reader() ->
     fun(FS) ->
             case file:read(FS, ?BLOCK_SIZE) of
@@ -119,7 +149,10 @@ file_reader() ->
             end
     end.
 
--spec block_chopper(BlockSize :: pos_integer()) -> reader_fun(binary()).
+-spec block_chopper(BlockSize) -> Reader when
+      BlockSize :: pos_integer(),
+      Reader :: reader_fun(State),
+      State :: binary().
 block_chopper(BlockSize) when is_integer(BlockSize), BlockSize > 0 ->
     fun(<<>>) ->
             {eof, <<>>};
@@ -129,14 +162,22 @@ block_chopper(BlockSize) when is_integer(BlockSize), BlockSize > 0 ->
             {Bin, <<>>}
     end.
 
--spec accumulator() -> fun((callback_message(), [row()]) -> [row()]).
+-spec accumulator() -> CallBack when
+      CallBack :: fun((Input, RevRows) -> Rows),
+      Input :: callback_message(),
+      RevRows :: rows(),
+      Rows :: rows().
 accumulator() -> fun acc/2.
 
--spec write_lines([line()]) -> iolist().
+-spec write_lines(Lines) -> Result when
+      Lines :: [line()],
+      Result :: iolist().
 write_lines(L) ->
     ecsv_nif:write_lines(L).
 
--spec write(line()) -> iolist().
+-spec write(Line) -> Result when
+      Line :: line(),
+      Result :: iolist().
 write(L) ->
     ecsv_nif:write(L).
 
@@ -160,6 +201,6 @@ parse_stream_(RF, RS, CF, CS, State) ->
             parse_stream_(RF, RS2, CF, CS2, S2)
     end.
 
--spec acc(callback_message(), [row()]) -> [row()].
+-spec acc(callback_message(), rows()) -> rows().
 acc({eof, X}, Acc) -> lists:reverse(X ++ Acc);
 acc({rows, X}, Acc) -> X ++ Acc.
